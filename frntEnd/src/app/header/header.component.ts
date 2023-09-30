eb6da2db1648a8724087c6d78a53bacf700c6e06
tarunKundlas <tkundlas6@gmail.com>
2023-09-30 12:41:02 +0530

    second commit

--- frntEnd/src/app/header/header.component.ts
+++ frntEnd/src/app/header/header.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
+  @HostListener('window:scroll', [])
+  onScroll() {
+    const header = this.el.nativeElement.querySelector('#header');
+    const lol = this.el.nativeElement.querySelector('#lol');
+
+    if (window.scrollY > 0) {
+      this.renderer.addClass(header, 'active');
+      lol.src = 'pic/bulb2.png';
+    } else {
+      this.renderer.removeClass(header, 'active');
+      lol.src = 'pic/bulb.png';
+    }
+  }
 }
